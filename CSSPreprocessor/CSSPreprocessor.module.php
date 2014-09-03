da6e7aaff6dda7666f251a56be2fc03793ab5a22
airelibre <contact@airelibre.fr>
2014-09-03 07:43:04 +0200

    Update 1.0.1

--- CSSPreprocessor/CSSPreprocessor.module.php
+++ CSSPreprocessor/CSSPreprocessor.module.php
@@ -28,7 +28,7 @@ if( !isset($gCms) ) exit;
 class CSSPreprocessor extends CMSModule 
 {
 	
-	function GetVersion() {return '1.0';}
+	function GetVersion() {return '1.0.1';}
 	function MinimumCMSVersion() {return '1.11';}
 	function GetFriendlyName() {return $this->Lang('friendlyname');}
 	function GetHelp(){return $this->Lang('help');}
