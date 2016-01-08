c0361f76fcecb5b0b65928e660c87c58cefadc4c
airelibre <contact@airelibre.fr>
2016-01-08 15:08:32 +0100

    Version 1.4

---
+++ CSSPreprocessor/lang/ext/nb_NO.php
@@ -0,0 +1,14 @@
+<?php
+$lang['cancel'] = 'Avbryt';
+$lang['css_preprocessor'] = 'CSS Preprocessor';
+$lang['choose_preprocessor'] = 'Velg den preprosessoren du vil benytte';
+$lang['friendlyname'] = 'CSS Preprocessor';
+$lang['generate_sourcemap'] = 'kildekart';
+$lang['generate_sourcemap_label'] = 'Generer kildekart';
+$lang['minify'] = 'Minimer';
+$lang['note'] = 'Merknad:';
+$lang['options'] = 'Valg';
+$lang['preferences'] = 'Preferanser';
+$lang['preferences_set'] = 'Preferansene ble lagret<br>Vennligst tøm ditt nettsteds mellomlager for å re-prosessere CSS';
+$lang['submit'] = 'Lagre';
+?>
---
+++ CSSPreprocessor/lang/ext/nl_NL.php
@@ -0,0 +1,8 @@
+<?php
+$lang['cancel'] = 'Annuleer';
+$lang['generate_sourcemap'] = 'Bronmap';
+$lang['options'] = 'Opties';
+$lang['preferences'] = 'Instellingen';
+$lang['submit'] = 'Versturen';
+$lang['ga'] = 'GA1.2.767965020.1400612364';
+?>
