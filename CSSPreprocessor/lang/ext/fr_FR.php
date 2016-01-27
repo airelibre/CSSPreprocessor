0507e14082db8472b11882ff3954e1a47faafc02
airelibre <contact@airelibre.fr>
2016-01-27 09:30:01 +0100

    Version 2.0

--- CSSPreprocessor/lang/ext/fr_FR.php
+++ CSSPreprocessor/lang/ext/fr_FR.php
@@ -6,18 +6,32 @@ $lang['choose_preprocessor'] = 'Choisissez le préprocesseur que vous souhaitez 
 $lang['friendlyname'] = 'Préprocesseur CSS';
 $lang['generate_sourcemap'] = 'Fichier sourcemap';
 $lang['generate_sourcemap_label'] = 'Générer un source map, si le préprocesseur le supporte';
+$lang['import_dirs'] = 'Répertoires contenant des fichiers LESS à inclure lors de la compilation - Relatif au répertoire racine du CMS - Liste séparée par des virgules';
+$lang['minify'] = 'Minifier';
+$lang['minify_label'] = 'Minifier le CSS';
+$lang['note'] = 'Note :';
+$lang['options'] = 'Options&nbsp;';
+$lang['preferences'] = 'Préférences';
+$lang['preferences_set'] = 'Préférences sauvegardées<br>Veuillez vider le cache du CMS pour re-générer les CSS';
+$lang['submit'] = 'Envoyer';
+$lang['use_autoprefixer'] = 'Utiliser Autoprefixer (plus d\'infos sur <a href="https:#github.com/postcss/autoprefixer" target="_blank">https:#github.com/postcss/autoprefixer</a>) - Seulement si installé sur le serveur et si le préprocesseur sélectionné le permet.';
 $lang['help'] = '<h2>What does this do?</h2>
- <p>This module adds to the CMSMS Stylesheets / Design manager some CSS Preprocessors, in order to compile LESS (or other maybe later) code</p>
- 
+ <p>This module adds to the CMSMS Stylesheets / Design manager some CSS Preprocessors, in order to compile LESS or SASS languages (or other maybe later).</p>
+
  <h2>How does it work and integrate in CMSMS</h2>
  <p>
 	This module uses the standard stylesheets of CMS Made Simple. It is launched after all the stylesheets have been combined, and it takes all the CSS content to give it to the Preprocessor of your choice.
  </p>
  <p>
-		Keep in mind that this module processes the entire stylesheet after their combination. No matter if you use 1 or 10 stylesheets in your design, the preprocessor will only get one string.
+		<strong>For CMSMS 1.12</strong> : Keep in mind that this module processes the entire stylesheet after their combination. No matter if you use 1 or 10 stylesheets in your design, the preprocessor will only get one string.
+  </p>
+  <p>
+    <strong>For CMSMS 2+</strong> : the new {cms_stylesheet} has a different behaviour. Every stylesheet is processed seperately, and the CSSPreprocessor module will not work if you have more than 1 LESS / SASS Stylessheet in the design. You will not be able to define a variable in one stylesheet, and then use it in another one.<br>
+    <strong>Solution:</strong> : put your LESS / SASS code in files and link the main file with the module through the "import dirs" parameter.
  </p>
 
 <h2>How Do I Use It</h2>
+<h3>CMSMS 1.12</h3>
 <ul>
 	<li>Use the "combine" option (default in {cms_stylesheet})</li>
 	<li>In order to recognize which template is a stylesheet, you MUST put that string in one of your stylesheets: <strong>@@CSSPreprocessor@@</strong> - I suggest putting it in a simple comment</li>
@@ -25,11 +39,33 @@ $lang['help'] = '<h2>What does this do?</h2>
 		Define your import directories, if you use it. For exemple, if you want to integrate Bootstrap, you can :
 		<ol>
 			<li>Put your Bootstrap LESS source files in uploads/bootstrap/</li>
-			<li>Type <strong><em>uploads/bootstrap</em></strong> in the "Directories containing LESS files to include in compilation" - No slash at beginning or end</li>
+			<li>Type <strong><em>uploads/bootstrap</em></strong> in the "Directories containing LESS/SASS files to include in compilation" - No slash at beginning or end</li>
 		</ol>
 	</li>
 	<li>Start using LESS code in your stylesheets!</li>
 </ul>
+
+<h3>CMSMS 2+</h3>
+<ul>
+	<li>In order to recognize which template is a stylesheet, you MUST put that string in one of your stylesheets: <strong>@@CSSPreprocessor@@</strong> - I suggest putting it in a simple comment</li>
+	<li>
+		Define your import directories, if you use it. For exemple, if you want to integrate Bootstrap, you can :
+		<ol>
+			<li>Put your Bootstrap LESS source files in uploads/bootstrap/</li>
+			<li>Type <strong><em>uploads/bootstrap</em></strong> in the "Directories containing LESS/SASS files to include in compilation" - No slash at beginning or end</li>
+      <li>Create a "custom" stylesheet (only one stylesheet) and link it to your design.</li>
+		</ol>
+	</li>
+  <li>
+		Same can be done for Foundation:
+		<ol>
+			<li>Put your Foundation SASS source files in uploads/foundation/scss</li>
+			<li>Type <strong><em>uploads/foundation/scss</em></strong> in the "Directories containing LESS/SASS files to include in compilation" - No slash at beginning or end</li>
+		</ol>
+	</li>
+	<li>Start using LESS code in your stylesheets!</li>
+</ul>
+
 
 <h2>Updating stylesheets</h2>
 <p>
@@ -37,22 +73,6 @@ $lang['help'] = '<h2>What does this do?</h2>
 </p>
 <p>
 	However, if you did make a change in your imported less files, the system will not perform any update - in this case, you must clear the cache or simply remove the stylesheet from the cache directory.
-</p>
-
-<h2>Example: using Bootstrap</h2>
-<p>This is a proposal on how to use and compile Bootstrap for CMS Made Simple with the CSSPreprocessor module :</p>
-<ol>
-	<li>Put the Bootstrap LESS files in <strong>uploads/bootstrap/</strong></li>
-	<li>Create a new stylesheet in CMSMS called something like <strong><em>Bootstrap :: main</em></strong></li>
-	<li>In that new stylesheet, copy all the content from bootstrap/bootstrap.less - This file is only a list of file import, but it\'s nice to have it in CMSMS because you can easily comment out the files you don\'t need</li>
-	<li>In the CSSPreprocessor module preferences, add <strong>uploads/bootstrap</strong> to the import directories</li>
-	<li>Create a new stylesheet called something like <strong><em>Custom :: main</em></strong></li>
-	<li>Put the string <strong>@@CSSPreprocessor@@</strong> at the top</li>
-	<li>You can now use that file to define your custom variables, override Bootstrap variables, create your styles, ... Or your can create as much stylesheets as you want</li>
-	<li>Don\'t forget to link your stylesheets to your templates (cmsms < 2.0) or designs (cmsms =< 2.0) - You should maybe put the Bootstrap :: main file at the top, before other stylesheets</li>
-</ol>
-<p>
-	Now you can simply change the grid or whatever you want in your custom files, because the preprocessors doesn\'t care of the order of the code - Putting <strong>@grid-columns: 12;</strong> in your custom stylesheets will work!
 </p>
 
 
@@ -86,7 +106,7 @@ $lang['help'] = '<h2>What does this do?</h2>
 	<p>
 		If you want to contact me: <a href="mailto:contact@airelibre.fr">contact@airelibre.fr</a>
 	</p>
-	
+
 <h2>Project on the web</h2>
 <p>You can follow the project on : </p>
 	<ul>
@@ -97,19 +117,10 @@ $lang['help'] = '<h2>What does this do?</h2>
 <h2>Copyright and License</h2>
 	<p>Copyright © 2014, AireLibre <a href="mailto:contact@airelibre.fr">contact@airelibre.fr</a> / <a href="http:#www.airelibre.fr" target="_blank">www.airelibre.fr</a>. All Rights Are Reserved.</p>
 	<p>This module has been released under the <a href="http:#www.gnu.org/licenses/licenses.html#GPL">GNU Public License</a>. You must agree to this license before using the module.</p>
-	
+
 	<h3>Preprocessors copyright</h3>
 	<p>
 		The author of this module does not own the preprocessors bundled with it - Please see every preprocessors source code / website to get informations about licence and copyright.
 	</p>
 	<p>I just wanted to thank all the developers that contribute to these preprocessors which are great tools for designers!</p>';
-$lang['import_dirs'] = 'Répertoires contenant des fichiers LESS à inclure lors de la compilation - Relatif au répertoire racine du CMS - Liste séparée par des virgules';
-$lang['minify'] = 'Minifier';
-$lang['minify_label'] = 'Minifier le CSS';
-$lang['note'] = 'Note :';
-$lang['options'] = 'Options&nbsp;';
-$lang['preferences'] = 'Préférences';
-$lang['preferences_set'] = 'Préférences sauvegardées<br>Veuillez vider le cache du CMS pour re-générer les CSS';
-$lang['submit'] = 'Envoyer';
-$lang['use_autoprefixer'] = 'Utiliser Autoprefixer (plus d\'infos sur <a href="https:#github.com/postcss/autoprefixer" target="_blank">https:#github.com/postcss/autoprefixer</a>) - Seulement si installé sur le serveur et si le préprocesseur sélectionné le permet.';
 ?>
